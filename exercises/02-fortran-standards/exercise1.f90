72d841b4ada9ea6ad413a783ad1f6d9fb03eef25
Julien Sindt <J.Sindt@epcc.ed.ac.uk>
2025-05-06 15:24:33 +0100

    Adding exercises

--- exercises/02-fortran-standards/exercise1.f90
+++
@@ -1,9 +0,0 @@
-program exercise1
-
-  ! Improve the following program
-
-  integer :: ll = 1
-
-  print *, "The value of ll is: ", l1
-
-end program exercise1
--- exercises/08-modules/solutions-1/solution_program.f90
+++
@@ -1,8 +0,0 @@
-program solution_program
-
-  use solution_module
-  implicit none
-
-  print *, "pi: ", pi_gauss_legendre()
-
-end program solution_program
